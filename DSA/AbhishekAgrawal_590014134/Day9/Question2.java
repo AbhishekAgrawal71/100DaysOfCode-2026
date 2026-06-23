3bb01df71509fd33e63dc457d73355d5dced9263
AbhishekAgrawal71 <abhi713111@gmail.com>
2026-06-23 18:57:48 +0530

    Day 9 solution

---
+++ DSA/AbhishekAgrawal_590014134/Day9/Question2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String result = str.replace(" ", "");
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
